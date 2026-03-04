0f4088cbf671b8282702fa6db665c9c3926101cb
Joaquín Antonino <joaqantonino@gmail.com>
2026-03-03 22:43:55 -0300

    -

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar"
 import Hero from "./pages/Hero"
 import Projects from "./pages/Projects"
 import About from "./pages/About"
-import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Hero />
       <Projects />
       <About />
-      <Analytics />
     </div>
   )
 }
