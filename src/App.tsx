9199548ccabf9655d3e34487c49172cfb30d0761
Joaquín Antonino <joaqantonino@gmail.com>
2026-03-20 16:30:46 -0300

    remove components of projects page for rebuild it & feat: new minimalistic cursor

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import Navbar from "./components/Navbar"
 import Hero from "./pages/Hero"
 import Projects from "./pages/Projects"
 import About from "./pages/About"
-import { ReactLenis } from 'lenis/react'
+import ReactLenis from 'lenis/react'
+import AnimatedCursor from "react-animated-cursor"
 
 function App() {
   return (
     <div className="bg-slate-950 min-h-screen text-slate-200 selection:bg-cyan-500 selection:text-cyan-950">
+      <AnimatedCursor
+      innerSize={16}
+      outerSize={8}
+      color="	0, 146, 179"
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5} />
       <ReactLenis root />
       <Navbar />
       <Hero />
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,5 +1,3 @@
-import FloatingLines from "../components/FloatingLines";
-import TiltedCard from "../components/TiltedCard";
 import timerrcImg from "../assets/images/timerrc-screenshot.webp"
 import lmregionalesImg from "../assets/images/lmregionales-screenshot.webp"
 import { SiTypescript, SiJavascript, SiReact, SiTailwindcss } from "react-icons/si";
@@ -29,75 +27,6 @@ const Projects = () => {
     <section id="projects" className="relative w-full min-h-screen overflow-hidden bg-slate-950 flex flex-col items-center justify-center py-24">
       <div className="absolute top-0 left-0 w-full h-64 bg-linear-to-b from-slate-950 to-transparent z-10 pointer-events-none" />
       
-      <div className="absolute inset-0 z-0">
-        <FloatingLines 
-          linesGradient={["#99fffe","#00d1ff","#0090c1"]} 
-          lineCount={5}
-          lineDistance={5}
-          bendRadius={5}
-          bendStrength={-0.5}
-          parallax={true}
-        />
-      </div>
-
-      <div className="relative z-30 container mx-auto px-6">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-16 lg:gap-2 justify-items-center">
-          {projects.map((project) => (
-            <div key={project.id} className="w-full max-w-100">
-              <TiltedCard
-                imageSrc={project.image}
-                altText={project.title}
-                containerHeight="400px"
-                containerWidth="100%"
-                imageHeight="400px"
-                imageWidth="100%"
-                rotateAmplitude={6}
-                scaleOnHover={1.08}
-                showTooltip={false}
-                displayOverlayContent={true}
-                overlayContent={
-                  <div className="w-full h-100 p-6 flex flex-col justify-end rounded-[15px] overflow-hidden group">
-                    <div className="absolute inset-0 bg-linear-to-t from-slate-950 via-slate-950/65 md:via-slate-950/50 to-transparent rounded-[15px]" />
-                    
-                    <div className="relative z-10">
-                      <h3 className="text-2xl md:text-3xl font-black text-[#00d1ff] mt-1 tracking-tight italic">
-                        {project.title}
-                      </h3>
-                      
-                      <p className="text-slate-300 text-xs md:text-sm mt-3 font-medium leading-relaxed">
-                        {project.description}
-                      </p>
-                      
-                      <div className="flex items-center justify-between mt-5">
-                        <div className="flex items-center gap-4">
-                          <div className="h-0.5 w-12 bg-[#00d1ff] group-hover:w-16 transition-all duration-500 ease-out" />
-                          <div className="flex gap-3 text-slate-400 text-xl hover:cursor-crosshair">
-                            {project.technologies.map((Icon, index) => (
-                              <span key={index} className="hover:text-[#00d1ff] transition-colors duration-300">
-                                {Icon}
-                              </span>
-                            ))}
-                          </div>
-                        </div>
-
-                        <a 
-                          href={project.link} 
-                          target="_blank" 
-                          rel="noopener noreferrer"
-                          className="flex items-center gap-2 py-2 px-4 bg-[#00d1ff] text-slate-950 text-xs font-bold rounded-full hover:scale-105 transition-transform duration-300 tracking-wider"
-                        >
-
-                          <FiExternalLink size={14} />
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                }
-              />
-            </div>
-          ))}
-        </div>
-      </div>
       
       <div className="absolute bottom-0 left-0 w-full h-32 bg-linear-to-t from-slate-950 to-transparent z-10 pointer-events-none" />
     </section>
